a68aecdf0d85f5903073b646f70da82b5825bab9
TsarYurii <tsar.yurii.wm@gmail.com>
2021-10-29 11:01:20 +0300

    commit

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBxEvnzk3ePahiu3GpD1erD4Gw0jWzVWT0",
   authDomain: "quasartraining-49d30.firebaseapp.com",
   projectId: "quasartraining-49d30",
-  storageBucket: "quasartraining-49d30.appspot.com",
+  storageBucket: "gs://quasartraining-49d30.appspot.com",
   messagingSenderId: "927877166578",
   appId: "1:927877166578:web:c169fd86bcdb65d7d9276b",
   measurementId: "G-D9FM4SDJ1C",
@@ -15,6 +16,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 
 
